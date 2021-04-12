8069c97fbb4fd5e2b70850feb517b47b4edc2e86
Nick Cheng <nscheng@connect.ust.hk>
2021-04-12 14:58:02 +0800

    clean code

--- test.py
+++ test.py
@@ -1,50 +1,3 @@
-# example request data:
-# {
-#   "version": "1.0",
-#   "session": {
-#     "is_new": false,
-#     "session_id": "530xxxxxxx",
-#     "application": { "app_id": "xxxxxxxxxx" },
-#     "user": {
-#       "user_id": "Z05hZ==",
-#       "is_user_login": true,
-#       "gender": "unknown"
-#     }
-#   },
-#   "request": {
-#     "type": 0,
-#     "request_id": "1b2f490768a8d09d859a3",
-#     "timestamp": 1572508188915,
-#     "intent": {
-#       "query": "听30分钟海浪声",
-#       "score": 0.800000011920929,
-#       "complete": true,
-#       "domain": "openplatform",
-#       "confidence": 1,
-#       "skillType": "Custom",
-#       "sub_domain": "1004244",
-#       "app_id": "xxxxxxx",
-#       "request_type": "Start",
-#       "need_fetch_token": false,
-#       "is_direct_wakeup": false,
-#       "slots": "{\"intent_name\":\"set_whitenoise\",\"slots\":[{\"name\":\"whitenoise\",\"value\":\"海浪\",\"raw_value\":\"海浪声\"},{\"name\":\"time\",\"value\":\"{\\\"start\\\":\\\"2019-10-31T15:49:48\\\",\\\"end\\\":\\\"2019-10-31T16:19:48\\\"}\",\"raw_value\":\"30分钟\"}]}"
-#     },
-#     "locale": "zh-CN",
-#     "slot_info": {
-#       "intent_name": "set_whitenoise",
-#       "slots": [
-#         { "name": "whitenoise", "value": "雨声", "raw_value": "海浪声" },
-#         {
-#           "name": "time",
-#           "value": "{\"start\":\"2019-10-31T15:09:48\",\"end\":\"2019-10-31T16:29:48\"}",
-#           "raw_value": "30分钟"
-#         }
-#       ]
-#     }
-#   },
-#   "query": "听30分钟海浪声",
-#   "context": { "device_id": "OQ+VrIRwXudjS+uT9KOuAA==", "in_exp": false }
-# }
 def extractInfo(data):
   return {
       "session_id": data["session"]["session_id"],
@@ -52,42 +5,8 @@ def extractInfo(data):
       # "request_id": data["request"]["request_id"]
   }
 
-
-# example response data:
-# {
-#   "is_session_end":false,
-#   "version": "1.0",
-#   "response":{
-# 	"confidence":0.8869365,
-# 	"open_mic":true,
-# 	"to_speak":{
-# 		"type":0,
-# 		"text":"海浪的声音来咯"},
-# 	"to_display":{
-# 		"type":0,
-# 		"text":"海浪的声音来咯"},
-# 	  "log_info":{}
-# 	},
-#   "session_attributes":{
-# 	"noticeFlag":1,
-# 	"replyKeyWord":"",
-# 	"turn":3,
-# 	"miniSkillInfo":{"name":"LONGTAIL"},
-# 	"session":{
-# 		"sessionID":"f7579e8e-982e-4d4b-9387-980a3b09edef",
-# 		"skillName":"",
-# 		"skillSubName":"",
-# 		"turn":0,
-# 		"data":[{"query":"海浪声","reply":"海浪的声音来咯",
-# 		"engine":"LONGTAIL"}]
-# 		},
-# 	"longtailEngine":"SKILL",
-# 	"replace":false,
-# 	"latitude":40.61924,"longtitude":120.73009}
-# 	}
-# }
 def getResponse(info, hasUnlocked):
-  response = {
+  response = {  # example response
         "is_session_end":False,
         "version": "1.0",
         "response":{
@@ -103,43 +22,25 @@ def getResponse(info, hasUnlocked):
           },
           "log_info":{}        
         },
-      # "session_attributes":{
-      #   "noticeFlag":1,
-      #   "replyKeyWord":"",
-      #   "turn":3,
-      #   "miniSkillInfo":{"name":"LONGTAIL"},
-      #   "session":{
-      #     "sessionID":"f7579e8e-982e-4d4b-9387-980a3b09edef",
-      #     "skillName":"",
-      #     "skillSubName":"",
-      #     "turn":0,
-      #     "data":[{"query":"海浪声","reply":"海浪的声音来咯",
-      #     "engine":"LONGTAIL"}]
-      #     },
-      #   "longtailEngine":"SKILL",
-      #   "replace":False,
-      #   "latitude":40.61924,"longtitude":120.73009
-      #   }
       }
 
-  # response["session_attributes"]["session"]["sessionID"] = info["session_id"]
+  # copy session id from request
+  response["session_attributes"]["session"]["sessionID"] = info["session_id"]
 
   if hasUnlocked:
     response["response"]["to_speak"] = "海浪的声音"
   else:
     response["response"]["to_speak"] = "声音来咯?"
 
-  print('reeeesponse-------------')
+  print('----------------response---------------')
   print(response)
   return response
 
 
 from flask import Flask, request, make_response
-# from flask_ngrok import run_with_ngrok
 import random
 
 app = Flask(__name__)
-# run_with_ngrok(app)
 
 @app.route("/", methods=['POST'])
 def webhook():
@@ -153,6 +54,7 @@ def webhook():
         }
         return content, 406
     else:
+        print('----------------request-----------------')
         print(str(data))
 
         requestInfo = extractInfo(data)
@@ -162,10 +64,9 @@ def webhook():
 
         return content, 200
 
-
-# do unlock
+# do unlock action
 # return whether has unlocked successfully
 def unlock():
-  return random.uniform(0, 1) > 0.5
+  return random.uniform(0, 1) > 0.5  # dummy
 
 app.run(debug=True, port=80)
